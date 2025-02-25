5a744de18f2b40ddad52653bd23df6c0f24e3553
Jose Angel Hernandez Rubio <tiofrank3460@gmail.com>
2025-02-25 10:40:59 -0700

    Add files via upload

---
+++ Metodos/src/Metodos/Aritmetica.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Metodos;
+
+
+public class Aritmetica {
+    int sumar(int a,int b){
+        return a+b;
+    }
+}
---
+++ Metodos/src/Metodos/PruebaArimetica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Metodos;
+
+
+public class PruebaArimetica {
+    public static void main(String args[]){
+        Aritmetica a = new Aritmetica();
+        
+        int resultado =a.sumar(5,3);
+        
+        System.out.println("Resultado:"+resultado);
+    }
+}
